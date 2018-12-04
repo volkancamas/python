5d1cd54ee09baa57f328eea6e9d15ef71e7bdab5
volkancamas <volkan.camas@yahoo.com>
2018-12-04 10:08:30 +0300

    Update haftalık_bulten.py

--- haftalık_bulten.py
+++ haftalık_bulten.py
@@ -3,7 +3,7 @@ import urllib
 from collections import OrderedDict
 import json
 
-/* BRSA Public Weekly Journal */
+#BRSA Public Weekly Journal
 
 url = 'http://ebulten.bddk.org.tr/HaftalikBulten/tr/Gosterim/VeriGetir'
 
@@ -15,9 +15,9 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'}
 
 
-/* This part of query must be edit for current or selected date
-I put 16.11.2018 specific date for test.It should be customize for your purpose.
-*/
+#This part of query must be edit for current or selected date
+#I put 16.11.2018 specific date for test.It should be customize for your purpose.
+#
 payload = OrderedDict([('kalem[]', ['0']), ('baslangic', '16/11/2018'), ('bitis', '16/11/2018') , ('taraf[]' , ['10001'])])
  
 
@@ -26,6 +26,6 @@ response = requests.get(url, headers=headers ,  data=payload)
 
 bddk_result_json = response.json()
 
-/*TODO : Save file UTF-8 Format*/
+#TODO : Save file UTF-8 Format*/
 with open('haftalik_bulten_result.json', 'w') as outfile:  
     json.dump(bddk_result_json, outfile) 
